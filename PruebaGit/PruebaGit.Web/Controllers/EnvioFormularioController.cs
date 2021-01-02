6e86c6896c62ba0ae1aed85665037aaae06437a1
Danielv210796 <erik.vargas18@tectijuana.edu.mx>
2021-01-01 21:13:16 -0600

    configuracion de envio de correo smtp

---
+++ PruebaGit/PruebaGit.Web/Controllers/EnvioFormularioController.cs
@@ -0,0 +1,35 @@
+﻿using PruebaGit.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaGit.Web.Controllers
+{
+    public class EnvioFormularioController : Controller
+    {
+        // GET: EnvioFormulario
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Formulario(Formulario model)
+        {
+            //se van agregar las variables para enviar el mensaje 
+            //por medio de smtp
+
+            var mensaje = new MailMessage();
+            mensaje.Subject = model.Asunto;
+            mensaje.Body = model.Mensaje;
+            mensaje.To.Add(model.Destino);
+
+            mensaje.IsBodyHtml = true;
+            var smtp = new SmtpClient();
+
+
+        }
+    }
+}
---
+++ PruebaGit/PruebaGit.Web/Models/Formulario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PruebaGit.Web.Models
+{
+    public class Formulario
+    {
+        //Agregando los campos que se requieren para enviar el formulario
+        //al paciente para confirmarle la cita correspondiente
+        public string Destino { get; set; }
+        public string Asunto { get; set; }
+        public string Mensaje { get; set; }
+
+    }
+}
